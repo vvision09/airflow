4b7aae62f391febe294ad30db0794bd48c4155fa
hourot <vision.io09o@gmail.com>
2024-01-07 19:59:27 +0900

    plugins 폴더 추가

---
+++ plugins/shell/select_fruit.sh
@@ -0,0 +1,10 @@
+FRUIT=$1 
+if [ $FRUIT == APPLE ];then 
+	echo "You selected Apple"
+elif [ $FRUIT == ORANGE ];then 
+	echo "You selected Orange" 
+elif [ $FRUIT == GRAPE ];then 
+	echo "You selected GRAPE" 
+else 
+	echo "You selected other Fruit" 
+fi 
